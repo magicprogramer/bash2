3fef22499c471e7374bb39fa553b2c36439b623d
ahmed ezzat <ahmedalahly800@gmail.com>
2024-12-14 15:03:04 +0200

    update checkers.sh

--- checkers.sh
+++ checkers.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 function is_num
 {
-        if [ $# -eq 0 ]
+        if [ $# -ne  1 ]
         then
-                echo "you must provide an argument"
-                exit 1
+                echo "you must provide an argument that doesn't contain space"
+                return 0
         fi
         if [[ $1 =~ ^[0-9]+$  ]]
         then
@@ -13,5 +13,21 @@ function is_num
                 return 0
         fi
 
+}
+function is_char
+{
+	if [ $# -ne  1 ]
+	then
+		echo "you must provide an argument that doesn't contain space"
+		return 0
+	fi
+	if [[ ${#1} -eq 1 && $1 =~ ^[a-zA-Z]$ ]]
+	then
+		echo "char"
+		return 1
+	else
+		echo "not char"
+		return 0
+	fi
 }
 
--- select.sh
+++ select.sh
@@ -8,4 +8,15 @@ database_name=$1
 table_name=$2
 source checkers.sh
 declare -i n=0
-cat "db/$database_name/$table_name"
+head -n 1  "db/$database_name/$table_name"
+tail -n 1 "db/$database_name/$table_name"
+local n=$(head -n 1 "db/$database_name/$table_name" | grep -o "|" | wc -l)
+n=$((n+1))
+echo $n
+echo "you are required to enter the fields you will search by it or leave it empty to not search by it"
+for i in {1..$n}
+do
+	echo $i
+	echo "enter "  $(head -n 1 "db/$database_name/$table_name" | cut -d "|" -f "$i") 
+done
+local types=$(head -n 1 "db/$database_name/$table_name" | cut -d "|" -f 1-4)
